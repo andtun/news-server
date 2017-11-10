f0fc71231d7db59241ebc9cee773a97f48b42275
Andrey Tunyatkin <andtun@yandex.ru>
2017-11-11 01:29:16 +0300

    logo & some text

--- app.py
+++ app.py
@@ -4,13 +4,31 @@ import os
 from bottle import *
 
 
+def html(filename):
+    return static_file(filename+".html", root='.')
+
+
 @get("/")
 def main():
-    return static_file('index.html', root='.')
+    return html("main")
+
+
+# ==================== Get template files ===================
 
 @get("/<filename>")
-def getit(filename):
+def get_it(filename):
     return static_file(filename, root='.')
+
+
+@get("/<whatever>/<filename>")
+def get_it(whatever, filename):
+    return static_file(filename, root=whatever)
+
+
+@get("/<whatever>/<wherever>/<filename>")
+def get_it(whatever, wherever, filename):
+    return static_file(filename, root=whatever+"/"+wherever)
+
 
 # run the server
 run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
